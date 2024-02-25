e23e2b42aeee76e074358117151d5435d5daad68
Dina Polousova <dinapolousova@gmail.com>
2024-02-25 23:24:11 +0300

    ProductItem Component

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,10 +1,56 @@
 import React from 'react';
 import './ProductList.css';
+import ProductItem from '../ProductItem/ProductItem';
+import { useTelegram } from '../../hooks/useTelegram';
+import { useCallback, useEffect, useState } from 'react';
+
+const products = [
+	{id: '1', title: 'Potatoes', price: 75, description: 'сорт Импала'},
+	{id: '2', title: 'Tomatoes', price: 150, description: 'сорт Розовые'},
+	{id: '3', title: 'Баклажаны', price: 80, description: 'сорт Алмаз'},
+	{id: '4', title: 'Cucumbers', price: 120, description: 'сорт ТСХ'},
+]
+
+const getTotalPrice = (items = []) => {
+	return items.reduce((acc, item) => {
+		return acc += item.price
+	}, 0)
+}
 
 const ProductList = () => {
+	const [addedItems, setAddedItems] = useState([]);
+	const {tg} = useTelegram();
+	const onAdd = (product) => {
+		const alreadyAdded = addedItems.find(item => item.id === product.id);
+		let newItems = [];
+
+		if(alreadyAdded) {
+			newItems = addedItems.filter(item => item.id !== product.id);
+		} else {
+			newItems = [...addedItems, product];
+		}
+
+		setAddedItems(newItems)
+
+		if(newItems.length === 0) {
+			tg.MainButton.hide();
+		} else {
+			tg.MainButton.show();
+			tg.MainButton.setParams({
+				text: `Купить ${getTotalPrice(newItems)}`
+			})
+		}
+	}
 
 	return (
-		<div>
+		<div className={'list'}>
+			{products.map(item => (
+				<ProductItem
+					product={item}
+					onAdd={onAdd}
+					className={'item'}
+				/>
+			))}
 			Product List
 		</div>
 	);
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
-const webAppUrl = 'https://greenpatobot.netlify.app';
+//const webAppUrl = 'https://greenpatobot.netlify.app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
