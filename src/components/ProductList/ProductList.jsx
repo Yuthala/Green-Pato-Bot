8646304cf1391b631293cf6e70843492119b6c89
Dina Polousova <dinapolousova@gmail.com>
2024-02-27 18:46:13 +0300

    added imges in public folder

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -6,11 +6,12 @@ import { useCallback, useEffect, useState } from 'react';
 
 //Вынести в базу данных
 const products = [
-	{id: '1', title: 'Картофель', price: 75, description: 'сорт Импала, кг', img: "https://yandex.ru/images/search?from=tabbar&img_url=https%3A%2F%2Fsveklon.ru%2Fwp-content%2Fuploads%2F2018%2F01%2Fisolated-potatoes.jpeg&lr=213&pos=29&rpt=simage&text=%D0%BA%D0%B0%D1%80%D1%82%D0%BE%D1%84%D0%B5%D0%BB%D1%8C%20%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8"},
-	{id: '2', title: 'Томаты', price: 150, description: 'сорт Розовые, кг', img: "https://yandex.ru/images/search?img_url=https%3A%2F%2Fhortis.info%2Fwp-content%2Fuploads%2F2017%2F06%2FTomato-pink-Brandywine-1960x1176-retina.jpg&lr=213&pos=1&rpt=simage&source=serp&text=%D1%82%D0%BE%D0%BC%D0%B0%D1%82%20%D1%80%D0%BE%D0%B7%D0%BE%D0%B2%D1%8B%D0%B9%20%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8"},
-	{id: '3', title: 'Баклажаны', price: 80, description: 'сорт Алмаз, кг', img: "https://yandex.ru/images/search?img_url=https%3A%2F%2Fimg-s2.onedio.com%2Fid-5c41b07e5e06bdde37847f07%2Frev-0%2Fw-1200%2Fh-631%2Ff-jpg%2Fs-b48f695d4693818414f5b5963bb98c7944d507f4.jpg&lr=213&pos=15&rpt=simage&text=%D0%B1%D0%B0%D0%BA%D0%BB%D0%B0%D0%B6%D0%B0%D0%BD%20%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8"},
-	{id: '4', title: 'Огруцы', price: 120, description: 'сорт ТСХ, 600 г', img: "../../img/cucmber.jpg"},
+	{id: '1', title: 'Картофель', price: 75, description: 'сорт Импала, кг', img: "img/potatoes.jpg"},
+	{id: '2', title: 'Томаты', price: 150, description: 'сорт Розовые, кг', img: "img/tomatoes.jpg"},
+	{id: '3', title: 'Баклажаны', price: 80, description: 'сорт Алмаз, кг', img: "img/eggplant.jpg"},
+	{id: '4', title: 'Огруцы', price: 120, description: 'сорт ТСХ, 600 г', img: "img/cucumber.jpg"}
 ]
+
 
 const getTotalPrice = (items = []) => {
 	return items.reduce((acc, item) => {
