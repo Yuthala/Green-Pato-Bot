37c3bed43cd4ae9e5d589700bca9fa24c8d6a846
Igor Polousov <igorpolousov@gmail.com>
2024-02-27 13:52:59 +0300

    + const TelegramBot, + cons bot, + bot.on function

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 require('dotenv').config()
+
+const TelegramBot = require('node-telegram-bot-api');
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', (msg) => {
+    // Access to chat Id
+    const chatId = msg.chatId
+
+    // Send message to the chat 
+    bot.sendMessage(chatId, 'recived your message')
+})
