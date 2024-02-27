92646d983fea8693907c47722819eeecf426c91d
Igor Polousov <igorpolousov@gmail.com>
2024-02-27 19:22:59 +0300

    + bot constaant, + chatId constant

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 require('dotenv').config()
 
+// Changing max listeners for node
+require('events').EventEmitter.defaultMaxListeners = 0;
+
+// Creating telegram api const
 const TelegramBot = require('node-telegram-bot-api');
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true});
 
-bot.on('message', (msg) => {
+// WebApp URL
+const webAppURL = 'https://greenpatobot.netlify.app'
+
+bot.on('message', async (msg) => {
     // Access to chat Id
-    const chatId = msg.chatId
+    const chatId = msg.chat.id
+    const text = msg.text
+
+    
+    if (text === '/start') {
+       // Markup keyboard "Feedback form"
+       await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму обратной связи', {
+        reply_markup: {
+            keyboard: [
+                [{text: 'Заполнить форму обратной связи', web_app: {url: webAppURL + '/form'}}]
+            ]
+        }
+       })
+       
+       // Inline keyboard "Make order"
+       await bot.sendMessage(chatId, 'Сделать заказ', {
+        reply_markup: {
+            inline_keyboard: [
+                [{text: 'Сделать заказ', web_app: {url: webAppURL}}]
+            ]
+        }
+       })
+
+    }
 
     // Send message to the chat 
     bot.sendMessage(chatId, 'recived your message')
