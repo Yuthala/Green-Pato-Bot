ac57b5a94e9982497ccbb66da2b353166159d2ad
Dina Polousova <dinapolousova@gmail.com>
2024-02-29 16:33:09 +0300

    added qty to DB objext

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -2,20 +2,31 @@ import React from "react";
 import Button from "../Button/Button";
 import './ProductItem.css';
 
+
+
 const ProductItem = ({product, className, onAdd})=> {
 
 	const onAddHandler = () => {
 		onAdd(product);
 	}
  
+
 	return (
 		<div className={'product' + className}>
 			<div className={'img'}><img src={product.img}/></div>
 			<div className={'title'}>{product.title}</div>
 			<div className={'description'}>{product.description}</div>
-			<div className={'price'}>
-				<span>Стоимость: <b>{product.price}</b></span>
+			<div className={'price'}><span>Цена: <b>{product.price}</b></span><span> р.</span>
+			<div className='count'>
+				<div className="count_box">
+					<input type="number" className="count_input" min="1" max="10"/>
+				</div>
+				<div className="controls">
+					<button type="button" className="count_up">⌃</button>
+					<button type="button" className="count_down">⌄</button>
+				</div>
 			</div>
+		</div>
 			<Button className={'add-btn'} onClick={onAddHandler}>
 				Добавить в корзину
 			</Button>
