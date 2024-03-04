245e38b5b3bc32742728274029c66c4f85159e3c
Igor Polousov <igorpolousov@gmail.com>
2024-03-04 13:20:20 +0300

    + post /web_data

--- index.js
+++ index.js
@@ -105,6 +105,25 @@ bot.on('message', async (msg) => {
     
 })
 
+app.post('/web_data', async (req, res) => {
+    const {queryId, products = [], totalPrice} = req.body
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Успешная покупка',
+            input_message_content: {
+                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+            }
+
+        })
+        return res.status(200).json({}) 
+    } catch (e) {
+        return res.status(500).json({})
+    }
+
+})
+
 // Launch server
 const PORT = 8000
 app.listen(PORT, () => console.log('Server starded on PORT ' + PORT))
