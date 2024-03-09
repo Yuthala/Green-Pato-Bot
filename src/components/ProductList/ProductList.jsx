cdee2fba721ea2aa77218d431fa31b019d1e046c
Igor Polousov <igorpolousov@gmail.com>
2024-03-09 15:44:17 +0300

    fd

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -32,36 +32,40 @@ const ProductList = () => {
 	// 1.2 Передача данных в Telegram --
 	const onSendData = useCallback(() => {
 		// Объект для передачи в Telegram
-        const data = {
-            products: addedItems,
-			totalPrice: getTotalPrice(addedItems),
-			queryId
-        }
+        // const data = {
+        //     products: addedItems,
+		// 	totalPrice: getTotalPrice(addedItems),
+		// 	queryId
+        // }
+		const data = orderCartData
 	tg.sendData(JSON.stringify(data));
     }, [addedItems])
 
-	// useEffect( ()=> {
-	// 	tg.onEvent('mainButtonClicked', onSendData)
-	// 		return ()=> {
-	// 			tg.offEvent('mainButtonClicked', onSendData)
-	// 		}
-	// 	}, [onSendData])
+	useEffect( ()=> {
+		tg.onEvent('mainButtonClicked', onSendData)
+		
+		orderCartData = {
+			products: addedItems,
+			totalPrice: getTotalPrice(addedItems),
+			queryId
+		}
+			return ()=> {
+				tg.offEvent('mainButtonClicked', onSendData)
+			}
+		}, [onSendData])
 
 	
-	useEffect( ()=> {
-		tg.onEvent('mainButtonClicked', function() {
+	// useEffect( ()=> {
+	// 	tg.onEvent('mainButtonClicked', function() {
 			orderCartData = {
 				products: addedItems,
 				totalPrice: getTotalPrice(addedItems),
 				queryId
-			};
-			window.location.href = "https://greenpatobot.netlify.app/form"
+			}
+	// 		window.location.href = "https://greenpatobot.netlify.app/form"
 			
-		})
-		
-		//tg.onEvent('mainButtonClicked', onSendData) 
-		
-	},[])
+	// 	})
+	// },[])
 	
 
 	//КОРЗИНА
