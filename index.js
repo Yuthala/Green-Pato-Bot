85661f133b864aa9747ea7e98127215976db7b4e
Igor Polousov <igorpolousov@gmail.com>
2024-03-10 16:04:50 +0300

    ds

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Injecting enviroument 
 require('dotenv').config();
 
+const express = require('express');
+
+const cors = require('cors');
+
 // Changing max listeners for node
 require('events').EventEmitter.defaultMaxListeners = 0;
 
@@ -10,8 +14,13 @@ const TelegramBot = require('node-telegram-bot-api');
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true});
 
-// Web URL
+// Web URL 
 const webAppURL = 'https://greenpatobot.netlify.app'
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 bot.on('message', async (msg) => {
     // Access to chat Id
@@ -23,8 +32,9 @@ bot.on('message', async (msg) => {
     if (msg?.web_app_data?.data) {
         try {
         const data = JSON.parse(msg?.web_app_data.data)
-        console.log(data)
-
+        console.log(`Данные из формы обратной связи ${data}`)
+        //console.log(data.orderCartData.totalPrice)
+            
         // await bot.sendMessage(chatId, `Спасибо ${data?.name}, контактные данные получены`)
         // await bot.sendMessage(chatId, 'Ваш адрес: ' + data?.street)
         // await bot.sendMessage(chatId, 'Ваш номер телефона: ' + data?.phone)
@@ -46,3 +56,32 @@ bot.on('message', async (msg) => {
     }
 
 })
+
+
+app.post('/web-data', async (request, response) => {
+    //if(!request.body) return response.sendStatus(400);
+    //const data = JSON.parse(request)
+    console.log(`Данные post запрос ${request.body.products}`)
+    //response.send(request.body)
+
+    const {queryId, products = [], totalPrice} = request.body;
+    
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: request.body.queryId,
+            title: 'Успешная покупка',
+            input_message_content: {
+                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${request.body.totalPrice}, ${request.body.products.map(item => item.title).join(', ')}`
+            }
+        })
+        
+        return response.status(200).json({});
+    } catch (e) {
+        return response.status(500).json({})
+    }
+})
+
+const PORT = 8000;
+
+app.listen(PORT, () => console.log('server started on PORT ' + PORT))
