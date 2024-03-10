323de85509cadebf2d33be3d0181cc9842c28625
Igor Polousov <igorpolousov@gmail.com>
2024-03-10 15:07:39 +0300

    ds

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,33 +31,51 @@ const ProductList = () => {
 
 	const newLocal = 'http://89.111.141.36:8000/web-data';
 
-	const onSendData = useCallback(() => {
-        const data = {
-            products: addedItems,
-            totalPrice: getTotalPrice(addedItems),
-            queryId,
-        }
+	// const onSendData = useCallback(() => {
+    //     const data = {
+    //         products: addedItems,
+    //         totalPrice: getTotalPrice(addedItems),
+    //         queryId,
+    //     }
 
-			fetch(newLocal , {
-				method: 'POST',
-				mode: cors,
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify(data)
-			})
+	// 		fetch(newLocal , {
+	// 			method: 'POST',
+	// 			mode: cors,
+	// 			headers: {
+	// 				'Content-Type': 'application/json',
+	// 			},
+	// 			body: JSON.stringify(data)
+	// 		})
         
-    }, [addedItems])
+    // }, [addedItems])
 
-	useEffect( ()=> {
-		tg.onEvent('mainButtonClicked', onSendData)
-			return ()=> {
-				tg.offEvent('mainButtonClicked', onSendData)
-			}
-		}, [onSendData])
+	// useEffect( ()=> {
+	// 	tg.onEvent('mainButtonClicked', onSendData)
+	// 		return ()=> {
+	// 			tg.offEvent('mainButtonClicked', onSendData)
+	// 		}
+	// 	}, [onSendData])
+
+		function sendData() {
+			const data = {
+						products: addedItems,
+						totalPrice: getTotalPrice(addedItems),
+						queryId,
+					}
+
+					fetch(newLocal , {
+									method: 'POST',
+									mode: cors,
+									headers: {
+										'Content-Type': 'application/json',
+									},
+									body: JSON.stringify(data)
+								})
+		}
 
 	useEffect( ()=> {
 		tg.onEvent('mainButtonClicked', function() {
+			sendData()
 			window.location.href = "https://greenpatobot.netlify.app/form"
 		})
 	})
