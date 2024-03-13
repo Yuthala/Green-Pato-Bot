cbe472c779c84a6494fb45dac351880f6365a8fb
Yuthala <dinapolousova@gmail.com>
2024-03-13 16:40:49 +0300

    port 80

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
 	// Отслеживание объекта tg 
 	const {tg, queryId} = useTelegram();
 
-	const newLocal = 'http://89.111.141.36:3000/web-data';
+	const newLocal = 'http://89.111.141.36:80/web-data';
 	//const newLocal = 'https://jsonplaceholder.typicode.com/users';
 
 	const onSendData = () => {
